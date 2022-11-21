b8981e4889255a020e7a303a4c90f424d46022f2
jakub <xivan@stuba.sk>
2022-11-21 19:31:41 +0100

    fix ctrl_adress2

--- lsm6dsl/lsm6dsl.c
+++ lsm6dsl/lsm6dsl.c
@@ -96,7 +96,7 @@ uint8_t lsm6dsl_init(void)
 	lsm6dsl_write_byte(LSM6DSL_ADDRESS_CTRL1, ctrl1);
 
 	//gyro device config
-	uint8_t ctrl2 = lsm6dsl_read_byte(LSM6DSL_ADDRESS_CTRL1);
+	uint8_t ctrl2 = lsm6dsl_read_byte(LSM6DSL_ADDRESS_CTRL2);
 	ctrl2 &= 0x70;
 	lsm6dsl_write_byte(LSM6DSL_ADDRESS_CTRL2, ctrl2);
 	return status;
